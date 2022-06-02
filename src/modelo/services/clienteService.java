03ab5840e2d2c10ab1300c186fdb9fd273649635
Leonardo Andrés Gayón Espinel <leonardo.andresin@gmail.com>
2022-06-02 00:16:15 -0500

    DAO cliente
    falta arreglar

--- src/modelo/services/clienteService.java
+++ src/modelo/services/clienteService.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public interface clienteService {
 
-        public List<persona> consultar();
-        public int crear(persona personaDTO);
+        public List<cliente> consultar();
+        public int crear(cliente personaDTO);
         public int actualizar(cliente personaDTO);
         public int eliminar(cliente personaDTO);
 
--- src/modelo/services/personaService.java
+++ src/modelo/services/personaService.java
@@ -1,4 +1,13 @@
 package modelo.services;
 
+import modelo.dto.cliente;
+import modelo.dto.persona;
+
+import java.util.List;
+
 public interface personaService {
+    public List<persona> consultar();
+    public int crear(persona personaDTO);
+    public int actualizar(persona personaDTO);
+    public int eliminar(persona personaDTO);
 }
