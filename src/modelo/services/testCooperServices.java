aab9140c6e91646b937548c9e23f6848b4637b20
Leonardo Andrés Gayón Espinel <leonardo.andresin@gmail.com>
2022-06-04 18:07:39 -0500

    Create testCooperServices.java

---
+++ src/modelo/services/testCooperServices.java
@@ -0,0 +1,13 @@
+package modelo.services;
+
+import modelo.dto.persona;
+import modelo.tests_deportivos.testCooper;
+
+import java.util.List;
+
+public interface testCooperServices {
+    public List<testCooper> consultar();
+    public int crear(testCooper cooperDTO);
+    public int actualizar(testCooper cooperDTO);
+    public int eliminar(testCooper cooperDTO);
+}
